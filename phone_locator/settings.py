61a7603b25da18c1e1864ff7f7f13ee0295d4f40
Nikolay Bayborodin <nbbrdn@gmail.com>
2024-01-25 06:05:21 +1000

    Add bootstrap css

--- phone_locator/settings.py
+++ phone_locator/settings.py
@@ -38,7 +38,9 @@ ROOT_URLCONF = "phone_locator.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            os.path.join(BASE_DIR, "templates"),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -83,6 +85,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "assets/"),)
+STATIC_DIR = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"

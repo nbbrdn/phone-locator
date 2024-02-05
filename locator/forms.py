751d1ac48e55071781bd2e8187226e4032aba7e3
Nikolay Bayborodin <nbbrdn@gmail.com>
2024-02-05 13:24:31 +1000

    Add phone number validator

--- locator/forms.py
+++ locator/forms.py
@@ -1,5 +1,15 @@
 from django import forms
+from django.core.validators import RegexValidator
 
 
 class PhoneNumberForm(forms.Form):
-    phone_number = forms.CharField(label="Enter phone number")
+    phone_number = forms.CharField(
+        label="Enter phone number",
+        validators=[
+            RegexValidator(
+                regex=r"^\d{11}$",
+                message="Phone number must be 11 digits.",
+                code="invalid_phone_number",
+            )
+        ],
+    )
--- locator/views.py
+++ locator/views.py
@@ -6,17 +6,20 @@ from .models import DEFCode
 
 def index(request):
     if request.method == "POST":
-        phone_number = request.POST.get("phone_number", "")
-        if phone_number.startswith("7"):
-            phone_number = phone_number[1:]
-        def_code = DEFCode.objects.get(
-            code=int(phone_number[:3]),
-            start_range__lte=int(phone_number[3:]),
-            end_range__gte=int(phone_number[3:]),
-        )
-        operator, region = def_code.operator, def_code.region
-        return render(
-            request, "search-result.html", {"operator": operator, "region": region}
-        )
+        form = PhoneNumberForm(request.POST)
+        if form.is_valid():
+            phone_number = request.POST.get("phone_number", "")
+            if phone_number.startswith("7"):
+                phone_number = phone_number[1:]
+            def_code = DEFCode.objects.get(
+                code=int(phone_number[:3]),
+                start_range__lte=int(phone_number[3:]),
+                end_range__gte=int(phone_number[3:]),
+            )
+            operator, region = def_code.operator, def_code.region
+            return render(
+                request, "search-result.html", {"operator": operator, "region": region}
+            )
+
     context = {"form": PhoneNumberForm}
     return render(request, "index.html", context)

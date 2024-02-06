270a725b301c173320dc5cbccb7c9eaaec4857f1
Nikolay Bayborodin <nbbrdn@gmail.com>
2024-02-06 13:45:24 +1000

    Add not found page

--- src/locator/views.py
+++ src/locator/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 
 from .forms import PhoneNumberForm
 from .models import DEFCode
@@ -12,21 +12,21 @@ def index(request):
             if phone_number.startswith("7"):
                 phone_number = phone_number[1:]
 
-            def_code = get_object_or_404(
-                DEFCode,
-                code=int(phone_number[:3]),
-                start_range__lte=int(phone_number[3:]),
-                end_range__gte=int(phone_number[3:]),
-            )
+            try:
+                def_code = DEFCode.objects.get(
+                    code=int(phone_number[:3]),
+                    start_range__lte=int(phone_number[3:]),
+                    end_range__gte=int(phone_number[3:]),
+                )
 
-            operator, region = def_code.operator, def_code.region
-            return render(
-                request, "search-result.html", {"operator": operator, "region": region}
-            )
+                operator, region = def_code.operator, def_code.region
+                return render(
+                    request,
+                    "search-result.html",
+                    {"operator": operator, "region": region},
+                )
+            except DEFCode.DoesNotExist:
+                return render(template_name="404.html", status=404, request=request)
 
     context = {"form": PhoneNumberForm}
     return render(request, "index.html", context)
-
-
-def not_found(request, exception):
-    return render(request, "404.html", status=404)
